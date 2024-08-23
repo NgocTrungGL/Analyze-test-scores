fecda08a8de669617759390817eec3a6d19a5812
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-08-23 10:36:07 +0700

    curl_data

---
+++ Data_Collection/curl_web.py
@@ -0,0 +1,8 @@
+# import subprocess 
+
+# result = subprocess.check_output('curl -F "sobaodanh=02000001" diemthi.hcm.edu.vn/Home/Show')
+
+# print(result)
+
+file = open("data.txt", "w")
+file.write("DUT Chao don cac ban TSV")
